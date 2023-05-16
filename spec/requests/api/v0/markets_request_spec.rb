6f300a5464809b2a5959dde7bf9123a105567c13
margdaly <margaret.daly@gmail.com>
2023-05-16 13:53:16 -0600

    test: Add happy path for get single market

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -46,4 +46,51 @@ describe 'Markets API' do
       expect(market[:vendor_count]).to be_an(Integer)
     end
   end
+
+  describe 'happy path' do
+    it 'sends a single market by its id' do
+      id = create(:market).id
+
+      get "/api/v0/markets/#{id}"
+
+      market = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to be_successful
+
+      expect(market[:data]).to have_key(:id)
+      expect(market[:data][:id]).to eq(id.to_s)
+
+      expect(market[:data]).to have_key(:attributes)
+      expect(market[:data][:attributes]).to be_a(Hash)
+
+      market = market[:data][:attributes]
+
+      expect(market).to have_key(:name)
+      expect(market[:name]).to be_a(String)
+
+      expect(market).to have_key(:street)
+      expect(market[:street]).to be_a(String)
+
+      expect(market).to have_key(:city)
+      expect(market[:city]).to be_a(String)
+
+      expect(market).to have_key(:county)
+      expect(market[:county]).to be_a(String)
+
+      expect(market).to have_key(:state)
+      expect(market[:state]).to be_a(String)
+
+      expect(market).to have_key(:zip)
+      expect(market[:zip]).to be_a(String)
+
+      expect(market).to have_key(:lat)
+      expect(market[:lat]).to be_a(String)
+
+      expect(market).to have_key(:lon)
+      expect(market[:lon]).to be_a(String)
+
+      expect(market).to have_key(:vendor_count)
+      expect(market[:vendor_count]).to be_an(Integer)
+    end
+  end
 end
