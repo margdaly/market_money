8f69ec233a466ceef0f087760134e7b07db0afff
margdaly <margaret.daly@gmail.com>
2023-05-16 16:48:00 -0600

    fix: Add test_data and remove validations

--- app/models/market.rb
+++ app/models/market.rb
@@ -2,8 +2,6 @@ class Market < ApplicationRecord
   has_many :market_vendors
   has_many :vendors, through: :market_vendors
 
-  validates_presence_of :id
-  
   def vendor_count
     self.vendors.count
   end
--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -6,25 +6,16 @@ RSpec.describe Market do
     it { should have_many(:vendors).through(:market_vendors) }
   end
 
-  describe 'validations' do
-    it { should validate_presence_of(:id) }
-  end
-
   describe 'instance methods' do
     describe '.vendor_count' do
       it 'returns the number of vendors at a market' do
-        market1 = create(:market, { id: 1})
-        vendor1 = create(:vendor)
-        vendor2 = create(:vendor)
-        vendor3 = create(:vendor)
-        create(:market_vendor, market_id: market1.id, vendor_id: vendor1.id)
-        create(:market_vendor, market_id: market1.id, vendor_id: vendor2.id)
+        test_data
 
-        expect(market1.vendor_count).to eq(2)
+        expect(@market1.vendor_count).to eq(2)
 
-        create(:market_vendor, market_id: market1.id, vendor_id: vendor3.id)
+        create(:market_vendor, market_id: @market1.id, vendor_id: @vendor3.id)
 
-        expect(market1.vendor_count).to eq(3)
+        expect(@market1.vendor_count).to eq(3)
       end
     end
   end
