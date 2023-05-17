8b959934f717798008c7e6aa30e86d26ae696a0d
margdaly <margaret.daly@gmail.com>
2023-05-17 11:03:23 -0600

    Add sad path for delete vendor

--- spec/requests/api/v0/vendors_request_spec.rb
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -219,5 +219,24 @@ describe 'Vendors API' do
       expect(Vendor.count).to eq(2)
       expect(MarketVendor.count).to eq(1)
     end
+
+    scenario 'sad path' do
+      invalid_id = 465734683123123
+
+      delete "/api/v0/vendors/#{invalid_id}"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      could_not_find = JSON.parse(response.body, symbolize_names: true) 
+
+      expect(could_not_find).to have_key(:errors)
+      expect(could_not_find[:errors]).to be_an(Array)
+
+      not_found_error = could_not_find[:errors][0]
+
+      expect(not_found_error).to have_key(:detail)
+      expect(not_found_error[:detail]).to eq("Couldn't find Vendor with 'id'=465734683123123")
+    end
   end
 end
