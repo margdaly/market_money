360b89ac734529d6ea2b0b9b1a5cd167bfc2d594
margdaly <margaret.daly@gmail.com>
2023-05-18 14:11:42 -0600

    Add validations to market vendors

--- app/models/market_vendor.rb
+++ app/models/market_vendor.rb
@@ -1,4 +1,15 @@
 class MarketVendor < ApplicationRecord
   belongs_to :market
   belongs_to :vendor
+
+  validate :unique_association
+  
+  private
+
+  def unique_association
+    market_vendor = MarketVendor.find_by(market_id: market_id, vendor_id: vendor_id)
+    return unless market_vendor && self != market_vendor
+
+    errors.add(:base, "Market vendor association between market with market_id=#{market_id} and vendor_id=#{vendor_id} already exists")
+  end
 end
